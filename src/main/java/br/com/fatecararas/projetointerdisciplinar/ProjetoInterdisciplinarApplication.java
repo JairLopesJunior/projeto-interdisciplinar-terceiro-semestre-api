b5cde4b643d2d444ab2b4e15a81e53d396f20444
unknown <jair.lopes@fatec.sp.gov.br>
2022-05-08 00:51:05 -0300

    Criado a estrutura do projeto

---
+++ src/main/java/br/com/fatecararas/projetointerdisciplinar/ProjetoInterdisciplinarApplication.java
@@ -0,0 +1,14 @@
+package br.com.fatecararas.projetointerdisciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ProjetoInterdisciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoInterdisciplinarApplication.class, args);
+	}
+
+}
