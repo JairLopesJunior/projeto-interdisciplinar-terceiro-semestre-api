d6f6ae2ccdaba649b49a26203958b30e1bdfcaf5
unknown <jair.lopes@fatec.sp.gov.br>
2022-05-28 00:16:59 -0300

    Criado teste para o erro de illegalArgumentException do metodo login da Classe UserServiceImpl

--- src/main/java/br/com/fatecararas/projetointerdisciplinar/domain/entities/UserEntity.java
+++ src/main/java/br/com/fatecararas/projetointerdisciplinar/domain/entities/UserEntity.java
@@ -21,9 +21,9 @@ import java.util.List;
 public class UserEntity {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(generator = "increment")
-    @GenericGenerator(name = "increment", strategy = "increment")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(generator = "increment")
+    //@GenericGenerator(name = "increment", strategy = "increment")
     @Column(name = "id_User")
     private Long idUser;
 
--- src/test/java/br/com/fatecararas/projetointerdisciplinar/services/UserServiceImplTest.java
+++ src/test/java/br/com/fatecararas/projetointerdisciplinar/services/UserServiceImplTest.java
@@ -12,11 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -48,12 +50,28 @@ public class UserServiceImplTest {
         createduser.setName("Fulano");
         createduser.setPassword("12345678");
 
-        when(userRepository.save(createduser)).thenReturn(createduser);
+        when(this.userRepository.save(createduser)).thenReturn(createduser);
 
         // Ação
         UserEntity returneduser = this.userServiceImpl.save(createdUserDTO);
 
         // Validação
         assertThat(returneduser, is(equalTo(createduser)));
+    }
+
+    @Test
+    void login() {
+        // Cenário
+        String emailParam = null;
+        String password = "123mudar";
+
+        when(this.userRepository.findByEmail(emailParam)).thenReturn(Optional.empty());
+
+        // Ação e Validação
+        IllegalArgumentException returnedException = assertThrows(IllegalArgumentException.class,
+                () -> userServiceImpl.login(emailParam, password));
+
+        // Validação
+        assertThat(returnedException.getMessage(), is(equalTo("The email or password fields are incorrect.")));
     }
 }
